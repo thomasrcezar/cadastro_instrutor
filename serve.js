118e8f0e90015a48147531ce6cfdc64429ed64af
thomasrcezar <thomasrbcezar@gmail.com>
2020-07-31 15:45:12 -0300

    Inciando css, rotas, e browserSync

--- serve.js
+++ serve.js
@@ -2,7 +2,10 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
 const server = express();
+
+
 server.use(express.static('public'));
+server.use(routes);
 
 server.set("view engine","njk")
 
